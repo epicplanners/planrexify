f5424b72c596d0944276d8a656a5da71a3379864
Jan Donnerbauer <jandonnerbauer@Jans-MacBook-Pro.local>
2020-04-20 17:56:35 +0200

    Stundenplan aktualisiert

--- prototypes/design_prototypes/first_xml_calender_test/src/first_xml_calender_test/First_xml_calender_test.java
+++ prototypes/design_prototypes/first_xml_calender_test/src/first_xml_calender_test/First_xml_calender_test.java
@@ -9,20 +9,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
  * @author Jan Donnerbauer
  */
 public class First_xml_calender_test extends Application {
-    
+
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
+
         Scene scene = new Scene(root);
-        
         stage.setScene(scene);
         stage.show();
     }
@@ -33,5 +34,5 @@ public class First_xml_calender_test extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
